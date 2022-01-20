71bf185691cfe45bb295795844ad77d964b434ca
Arturo <arturo8a@gmail.com>
2022-01-19 21:01:11 -0500

    Added changes for basic node and express part

--- myApp.js
+++ myApp.js
@@ -1,7 +1,48 @@
+const bodyParser = require('body-parser')
 var express = require('express');
 var app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use("/",(req,res,next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
 
-console.log("Hello World")
+app.get("/",(req,res) => {
+  let absolutePath = __dirname + "/views/index.html"
+  res.sendFile(absolutePath)
+});
+
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString();
+  next();
+}, function(req, res) {
+  res.json({time: req.time});
+});
+
+app.get('/name', function(req, res) {
+  res.json({ name: `${req.query.first} ${req.query.last}`});
+});
+
+app.post('/name', function(req, res) {
+  const {first,last} = req.body;
+  res.json({ name: `${first} ${last}`});
+});
+
+app.get('/:word/echo', function(req, res) {
+  res.json({echo: req.params.word});
+});
+
+app.get("/json",(req,res) => {
+
+  const mySecret = process.env['MESSAGE_STYLE']
+  res.json({"message": mySecret == 'uppercase' ? "Hello json".toUpperCase() : "Hello json"});
+});
+
+
+
+app.use("/public",express.static( __dirname + "/public"));
+
+
 
 
 
